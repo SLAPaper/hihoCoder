d8233d7643da35dc42ff9e9871e20092ae4b4a8f
SLAPaper Pang <slapaper.pku@gmail.com>
2017-10-18 22:01:10 +0800

    feat: W172 Python TLE 80/100

--- 0000 hihoOnce/172 Matrix Sum/main.py
+++ 0000 hihoOnce/172 Matrix Sum/main.py
@@ -1,61 +1,57 @@
 from __future__ import print_function
 
 from array import array
-import sys
+
+BASE = int(1e9 + 7)
 
 
 class TreeMatrix:
-    def __init__(self, N, M):
-        self.mat = [array('i', [0] * (M + 1))] * (N + 1)
+    def __init__(self, N):
+        self.mat = [array('i', [0] * (N + 1)) for _ in range(N + 1)]
         self.N = N
-        self.M = M
 
     @staticmethod
     def lowbit(x):
         return x & (-x)
 
     def add(self, x, y, val):
+        x, y = x + 1, y + 1
+
         i = x
         while i <= self.N:
             j = y
-            while j <= self.M:
-                print(i, j, val)
-                print(self.mat)
-                self.mat[i][j] += val
+            while j <= self.N:
+                self.mat[i][j] = (self.mat[i][j] + val) % BASE
                 j += self.lowbit(j)
             i += self.lowbit(i)
 
     def _sum(self, x, y):
+        x, y = x + 1, y + 1
         ret = 0
         i = x
         while i > 0:
             j = y
             while j > 0:
-                ret += self.mat[i][j]
+                ret = (ret + self.mat[i][j]) % BASE
                 j -= self.lowbit(j)
             i -= self.lowbit(i)
         return ret
 
     def sum(self, x1, y1, x2, y2):
-        return self._sum(x1 - 1, y1 - 1) - self._sum(x1 - 1, y2) - self._sum(
-            x2, y1 - 1) + self._sum(x2, y2)
+        return (self._sum(x2, y2) - self._sum(x1 - 1, y2) - self._sum(
+            x2, y1 - 1) + self._sum(x1 - 1, y1 - 1)) % BASE
 
 
-def main(stdin=sys.stdin):
-    sys.stdin = stdin
-
+if __name__ == '__main__':
     N, M = (int(x) for x in raw_input().split())
 
-    tmat = TreeMatrix(N, N)
+    tmat = TreeMatrix(N)
 
     for _ in range(M):
         op, param = raw_input().split(None, 1)
         if op == 'Add':
             x, y, val = (int(x) for x in param.split())
-            tmat.add(x + 1, y + 1, val)
+            tmat.add(x, y, val)
         elif op == 'Sum':
             x1, y1, x2, y2 = (int(x) for x in param.split())
-            print(tmat.sum(x1 + 1, y1 + 1, x2 + 1, y2 + 1))
-
-
-main()
+            print(tmat.sum(x1, y1, x2, y2))
