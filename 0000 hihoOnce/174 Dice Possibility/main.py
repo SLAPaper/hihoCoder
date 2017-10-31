970cee33abee54852d8f0dcebd030d2d82e33999
SLAPaper Pang <slapaper.pku@gmail.com>
2017-11-01 01:48:48 +0800

    feat: W174 Python WA 50/100 6690ms 7MB

--- 0000 hihoOnce/174 Dice Possibility/main.py
+++ 0000 hihoOnce/174 Dice Possibility/main.py
@@ -1,41 +1,24 @@
 from __future__ import print_function, division
 from fractions import Fraction
 
-
-def cache(func):
-    memory = {}
-
-    def wrapper(n, m):
-        if (n, m) in memory:
-            return memory[(n, m)]
-        else:
-            result = func(n, m)
-            memory[(n, m)] = result
-            return result
-
-    return wrapper
+import sys
 
 
-@cache
 def calc(n, m):
-    if n == 1:
-        if m in set([1, 2, 3, 4, 5, 6]):
-            return Fraction(1, 6)
-        else:
-            return Fraction()
-
-    r1 = calc(n - 1, m - 1)
-    r2 = calc(n - 1, m - 2)
-    r3 = calc(n - 1, m - 3)
-    r4 = calc(n - 1, m - 4)
-    r5 = calc(n - 1, m - 5)
-    r6 = calc(n - 1, m - 6)
-
-    return (r1 + r2 + r3 + r4 + r5 + r6) / 6
+    s = [Fraction()] * (m + 1)
+    s[1:6] = [Fraction(1, 6)] * 6
+    for i in range(1, n):
+        ns = s[:]
+        for j in range(1, m + 1):
+            ns[j] = ((s[j - 1] if j - 1 > 0 else Fraction()) -
+                     (s[j - 7] if j - 7 > 0 else Fraction())) / 6 + ns[j - 1]
+        s = ns
+    return s[m]
 
 
 def main():
     n, m = (int(x) for x in raw_input().split())
+    sys.stdin.flush()
     print("%.2f" % float(calc(n, m) * 100))
 
 
